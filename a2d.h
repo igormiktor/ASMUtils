23a6ebce328591ffa6dd3c66240d98ccd9dffb0c
Igor Mikolic-Torreira <igormt@alumni.caltech.edu>
2015-11-01 15:46:11 -0500

    Add basic analog-to-digital functionality

---
+++ a2d.h
@@ -0,0 +1,48 @@
+/*
+    a2d.h - C interface to routines written in assembler
+
+    The MIT License (MIT)
+
+    Copyright (c) 2015 Igor Mikolic-Torreira
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+
+#ifndef a2dassem_h
+#define a2dassem_h
+
+#include <stdint.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void enableA2D();
+
+void setA2Dchannel( uint8_t channel );
+
+uint16_t readA2D();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
